d81015fb2e820a070674bcbde2dd2364047c3bd7
Tufazzul1 <tufajjul2017@gmail.com>
2024-05-10 02:23:03 +0600

    added the basic setup

---
+++ src/shared/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className="text-4xl">Hello from footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light"><h2 className="text-xl">Hi</h2></nav>
+        </div>
+    );
+};
+
+export default Navbar;
